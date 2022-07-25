035cb5f2d84c43a262458543d92e5bde7ec291f3
Murat Melih <muratmelihisik@gmail.com>
2022-07-25 13:59:34 +0300

    rate component and modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import ModalComponent from './components/rate-modal-component/rate-modal-component';
 
 function App() {
   return (
     <div className="App">
-   
+      <ModalComponent />
     </div>
   );
 }
